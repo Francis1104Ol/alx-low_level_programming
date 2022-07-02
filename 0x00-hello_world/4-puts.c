0a4f4fad977ffde609f819c191cb13bd8026c56e
Francis1104Ol <taiwofrancis001@gmail.com>
2022-07-01 17:11:44 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
-  /**
+
+/**
+ * main - Entry point
  *  * Description: main - Prints outs a sring.
+ *
  *   * Return:0 if success
+ *
  **/
+
 int main(void)
+
 {
+
 		puts("\"Programming is like building a multilingual puzzle");
+
 			return (0);
+
 }
