d8a25f4e3d0150392c46bd44cfcf1e0329b26996
Francis1104Ol <taiwofrancis001@gmail.com>
2022-07-01 17:35:30 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include < stdio.h >
 /**
  *  * main - prints out a string
- *   *
- *    * Return: 0 if success
+ *  *
+ *   * Return: 0 if success
  **/
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 
 }
