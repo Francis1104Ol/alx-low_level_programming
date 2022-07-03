8b26e2b5d7c90270e248f74ca16bbfd5d531a167
Francis1104Ol <taiwofrancis001@gmail.com>
2022-07-02 20:11:22 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int g, t;
+
+	for (g = '0'; a < 9; g++)
+	{
+		for (t = g + 1; t <= '9'; t++)
+		{
+			if (g != t)
+			{
+				putchar(g);
+				putchar(t);
+
+				if (g == '8' && t == '9')
+					continue;
+
+				putchar(',')
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
