4312d36391f092876136de7ffb4f8c218e4bfc96
Francis1104Ol <taiwofrancis001@gmail.com>
2022-07-02 19:11:42 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int a = 0;
+
+	for (a = 48; a <= 57; a++)
+
+	{
+		putchar(a);
+	}
+
+	putchar('\n');
+	return (0);
+}
