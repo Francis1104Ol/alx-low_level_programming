94657a011eac82e652f0daefc22a1a97a6e70c2c
Francis1104Ol <taiwofrancis001@gmail.com>
2022-07-05 15:29:55 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main"
+/**
+ * print_alphabet -print alphabet in lower case.
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch)
+	}
+	_putchar('\n');
+}
