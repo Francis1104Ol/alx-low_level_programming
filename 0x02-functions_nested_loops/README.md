0cabdedf926713b751a42b0a95aa0414724f4b91
Francis1104Ol <taiwofrancis001@gmail.com>
2022-07-05 15:12:18 -0700

    headerfile

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+# 0x02-functions_nested_loops
+
+# 0-putchar.c
+prints "putchar," followed by a new line.
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main -call other functions 
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	 0-putchar.c();
+
+	 return (0);
+}
