f7d6e6d0d23432c3c4417563390afeb7fde643aa
Francis1104Ol <taiwofrancis001@gmail.com>
2022-07-05 17:49:37 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - print the sum of two integers
+ * @a: is an integer parameter
+ * @b: is an integer parameter
+ * Return: 0 if success
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
