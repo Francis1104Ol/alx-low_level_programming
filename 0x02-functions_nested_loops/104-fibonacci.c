836b1eb7b5b5f104677f406e2c41257ac39a3ee5
Francis1104Ol <taiwofrancis001@gmail.com>
2022-07-06 07:34:11 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	int i = 1;
+	int i = 0;
 	long j = 1, k = 2;
 
-	while (i < 50)
+	while (i < 98)
 	{
-		if (i == 1)
+		if (i == 0)
 			printf("%ld", j);
 		else if (i == 1)
 			printf(", %ld", k);
