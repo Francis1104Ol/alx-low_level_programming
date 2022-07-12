3d4b67bb4b23ed114c8379c35c4f10275f7c07bb
Francis1104Ol <taiwofrancis001@gmail.com>
2022-07-12 09:45:13 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * reset_to_98.c - writing a funcion that update the value of it poin
+ * reset_to_98 - writing a funcion that update the value of it poin
  * @n: parameter of a function
  */
 
