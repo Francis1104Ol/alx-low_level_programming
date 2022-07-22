7422438aeb3f56cdfc9d2235e391654ccb64b79f
Francis1104Ol <taiwofrancis001@gmail.com>
2022-07-22 03:16:41 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * main - functions that multiiplies two number
  * @argv: argument value
  * @argc: argument count
+ *
  * Return: 1 if error and 0 success
  */
 int main(int argc, char *argv[])
 {
+	int a, b, c;
+
 	if (argc != 3)
 	{
-		printf("Error\n");
+		puts("Error");
 		return (1);
 	}
-	else
-	{
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-		return (0);
-	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	c = a * b;
+	printf("%d\n", c);
+	return (0);
 }
