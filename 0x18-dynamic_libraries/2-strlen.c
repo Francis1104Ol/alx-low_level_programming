f47fa77a426a2f44228d8e0a7d81b32e6511518d
Francis1104OL <taiwofrancis001@gmail.com>
2022-10-03 13:33:37 -0700

    2-strlen.c

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - length of a string
+ * @s: parameter
+ *
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+	count++;
+	s++;
+	}
+	return (count);
+}
