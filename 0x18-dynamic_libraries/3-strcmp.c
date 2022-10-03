c6d4d0bdbe328c6ef23ef8d189dae8c688451f30
Francis1104OL <taiwofrancis001@gmail.com>
2022-10-03 13:38:02 -0700

    3-strcmp.c

---
+++ 0x18-dynamic_libraries/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_strcmp - compares two strings
+ *@s1: first string
+ *@s2: second string
+ *Return: int value
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
