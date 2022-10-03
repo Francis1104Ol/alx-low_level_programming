bdb8fb32cdb45bf29785a7c0e150851113fe3474
Francis1104OL <taiwofrancis001@gmail.com>
2022-10-03 13:22:54 -0700

    0-strcat.c

---
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_strcat - concatenates two strings
+ *@dest: func para
+ *@src: second func para
+ *Return: resulting string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+
+	return (dest);
+}
