5f29a4a69b46adcfb0129d4fb7cad25701630f25
Francis1104OL <taiwofrancis001@gmail.com>
2022-10-21 14:24:34 -0700

    4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -7,18 +7,19 @@
 */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index = 0;
-	hash_node_t *Head;
+	unsigned long int index;
+	hash_node_t *tmp;
 
-	if (key == '\0' || key == NULL || ht == NULL)
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+	    key == NULL || strlen(key) == 0)
 		return (NULL);
-	index = key_index((unsigned char *)key, (*ht).size);
-	Head = (*ht).array[index];
-	while (Head != NULL)
+	index = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+	while (tmp != NULL)
 	{
-		if (strcmp((*Head).key, key) == 0)
-			return ((*Head).value);
-		Head = (*Head).next;
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
 	}
 	return (NULL);
 }
